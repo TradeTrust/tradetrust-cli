93bebc6cedbb9bb92411d372bd7570464ea56ae6
Haninn <han-inn.ng@sgtradex.com>
2024-08-15 12:27:23 +0800

    fix: linting

--- src/commands/dns/txt-record/__test__/get.test.ts
+++ src/commands/dns/txt-record/__test__/get.test.ts
@@ -9,24 +9,30 @@ describe("get", () => {
     await handler({ location: "tradetrust.io" });
 
     // dns-txt
-    expect(consoleSpy).toHaveBeenNthCalledWith(1, expect.arrayContaining([
-      expect.objectContaining({
-        addr: expect.stringMatching(/^0x[a-fA-F0-9]{40}$/g),
-        dnssec: expect.any(Boolean),
-        net: expect.stringMatching(/^[a-z]+$/),
-        netId: expect.stringMatching(/^[0-9]+$/),
-        type: 'openatts'
-      })
-    ]));
+    expect(consoleSpy).toHaveBeenNthCalledWith(
+      1,
+      expect.arrayContaining([
+        expect.objectContaining({
+          addr: expect.stringMatching(/^0x[a-fA-F0-9]{40}$/g),
+          dnssec: expect.any(Boolean),
+          net: expect.stringMatching(/^[a-z]+$/),
+          netId: expect.stringMatching(/^[0-9]+$/),
+          type: "openatts",
+        }),
+      ])
+    );
     // dns-did
-    expect(consoleSpy).toHaveBeenNthCalledWith(2, expect.arrayContaining([
-      expect.objectContaining({
-        algorithm: 'dns-did',
-        publicKey: expect.stringMatching(/^did:ethr:0x[a-fA-F0-9]{40}#controller$/g),
-        version: expect.stringMatching(/^[0-9.]+$/),
-        dnssec: expect.any(Boolean),
-        type: 'openatts'
-      })
-    ]));
+    expect(consoleSpy).toHaveBeenNthCalledWith(
+      2,
+      expect.arrayContaining([
+        expect.objectContaining({
+          algorithm: "dns-did",
+          publicKey: expect.stringMatching(/^did:ethr:0x[a-fA-F0-9]{40}#controller$/g),
+          version: expect.stringMatching(/^[0-9.]+$/),
+          dnssec: expect.any(Boolean),
+          type: "openatts",
+        }),
+      ])
+    );
   });
 });
