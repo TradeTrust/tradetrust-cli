28850af3d60c12b14a64c0553d44c62c0e5bf786
Haninn <han-inn.ng@sgtradex.com>
2024-08-15 12:09:17 +0800

    fix: resolve tradetrust-tt/tradetrust lib error and resolve test error

--- src/commands/dns/txt-record/__test__/get.test.ts
+++ src/commands/dns/txt-record/__test__/get.test.ts
@@ -7,9 +7,26 @@ describe("get", () => {
   it("should return dns-txt", async () => {
     const consoleSpy = jest.spyOn(console, "table");
     await handler({ location: "tradetrust.io" });
+
     // dns-txt
-    expect(consoleSpy).toMatchSnapshot();
+    expect(consoleSpy).toHaveBeenNthCalledWith(1, expect.arrayContaining([
+      expect.objectContaining({
+        addr: expect.stringMatching(/^0x[a-fA-F0-9]{40}$/g),
+        dnssec: expect.any(Boolean),
+        net: expect.stringMatching(/^[a-z]+$/),
+        netId: expect.stringMatching(/^[0-9]+$/),
+        type: 'openatts'
+      })
+    ]));
     // dns-did
-    expect(consoleSpy).toMatchSnapshot();
+    expect(consoleSpy).toHaveBeenNthCalledWith(2, expect.arrayContaining([
+      expect.objectContaining({
+        algorithm: 'dns-did',
+        publicKey: expect.stringMatching(/^did:ethr:0x[a-fA-F0-9]{40}#controller$/g),
+        version: expect.stringMatching(/^[0-9.]+$/),
+        dnssec: expect.any(Boolean),
+        type: 'openatts'
+      })
+    ]));
   });
 });
