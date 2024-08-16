7a617c0821dbdca8446335c5eb9d49d202c99845
Haninn <han-inn.ng@sgtradex.com>
2024-08-16 10:55:25 +0800

    fix: revert test case changes, sort test result.

--- src/commands/dns/txt-record/__test__/get.test.ts
+++ src/commands/dns/txt-record/__test__/get.test.ts
@@ -8,31 +8,19 @@ describe("get", () => {
     const consoleSpy = jest.spyOn(console, "table");
     await handler({ location: "tradetrust.io" });
 
+    const mockCall1 = consoleSpy.mock.calls[0][0];
+    mockCall1.sort((a: any, b: any) => {
+      if (a.netId < b.netId) return -1;
+      if (a.netId > b.netId) return 1;
+      if (a.addr < b.addr) return -1;
+      if (a.addr > b.addr) return 1;
+      return 0;
+    });
+    const mockCall2 = consoleSpy.mock.calls[1][0];
+    mockCall2.sort((a: any, b: any) => (a.publicKey < b.publicKey ? -1 : 1));
     // dns-txt
-    expect(consoleSpy).toHaveBeenNthCalledWith(
-      1,
-      expect.arrayContaining([
-        expect.objectContaining({
-          addr: expect.stringMatching(/^0x[a-fA-F0-9]{40}$/g),
-          dnssec: expect.any(Boolean),
-          net: expect.stringMatching(/^[a-z]+$/),
-          netId: expect.stringMatching(/^[0-9]+$/),
-          type: "openatts",
-        }),
-      ])
-    );
+    expect(mockCall1).toMatchSnapshot();
     // dns-did
-    expect(consoleSpy).toHaveBeenNthCalledWith(
-      2,
-      expect.arrayContaining([
-        expect.objectContaining({
-          algorithm: "dns-did",
-          publicKey: expect.stringMatching(/^did:ethr:0x[a-fA-F0-9]{40}#controller$/g),
-          version: expect.stringMatching(/^[0-9.]+$/),
-          dnssec: expect.any(Boolean),
-          type: "openatts",
-        }),
-      ])
-    );
+    expect(mockCall2).toMatchSnapshot();
   });
 });
