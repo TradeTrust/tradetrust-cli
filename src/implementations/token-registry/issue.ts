8b40c853a12105cc0e81dab61abae58c5b382db7
nghaninn <han-inn.ng@sgtradex.com>
2024-10-15 19:21:42 +0800

    chore: fix linting

--- src/implementations/token-registry/issue.ts
+++ src/implementations/token-registry/issue.ts
@@ -22,7 +22,7 @@ export const issueToTokenRegistry = async ({
   const tokenRegistry: TradeTrustToken = await TradeTrustToken__factory.connect(address, wallet);
   if (dryRun) {
     await dryRunMode({
-      estimatedGas: await tokenRegistry.estimateGas.mint(beneficiary, holder, tokenId, '0x'),
+      estimatedGas: await tokenRegistry.estimateGas.mint(beneficiary, holder, tokenId, "0x"),
       network,
     });
     process.exit(0);
@@ -33,13 +33,13 @@ export const issueToTokenRegistry = async ({
     const gasFees = await getGasFees({ provider: wallet.provider, ...rest });
     trace(`Gas maxFeePerGas: ${gasFees.maxFeePerGas}`);
     trace(`Gas maxPriorityFeePerGas: ${gasFees.maxPriorityFeePerGas}`);
-    await tokenRegistry.callStatic.mint(beneficiary, holder, tokenId, '0x');
+    await tokenRegistry.callStatic.mint(beneficiary, holder, tokenId, "0x");
     signale.await(`Sending transaction to pool`);
-    transaction = await tokenRegistry.mint(beneficiary, holder, tokenId, '0x', { ...gasFees });
+    transaction = await tokenRegistry.mint(beneficiary, holder, tokenId, "0x", { ...gasFees });
   } else {
-    await tokenRegistry.callStatic.mint(beneficiary, holder, tokenId, '0x');
+    await tokenRegistry.callStatic.mint(beneficiary, holder, tokenId, "0x");
     signale.await(`Sending transaction to pool`);
-    transaction = await tokenRegistry.mint(beneficiary, holder, tokenId, '0x');
+    transaction = await tokenRegistry.mint(beneficiary, holder, tokenId, "0x");
   }
 
   trace(`Tx hash: ${transaction.hash}`);
