d91ff921ba494420857bb6104b81378ef75bec84
moiz-sgtradex <moiz.shaikh@sgtradextech.com>
2025-07-21 12:09:53 +0530

    fix: token issue function

--- src/implementations/token-registry/issue.ts
+++ src/implementations/token-registry/issue.ts
@@ -1,12 +1,11 @@
-import { TradeTrustToken, TradeTrustToken__factory } from "@tradetrust-tt/token-registry/contracts";
 import signale from "signale";
 import { getLogger } from "../../logger";
 import { getWalletOrSigner } from "../utils/wallet";
 import { TokenRegistryIssueCommand } from "../../commands/token-registry/token-registry-command.type";
-import { dryRunMode } from "../utils/dryRun";
 import { TransactionReceipt } from "@ethersproject/providers";
 import { canEstimateGasPrice, getGasFees } from "../../utils";
-import { validateAndEncryptRemark } from "../title-escrow/helpers";
+import { BigNumber } from "ethers";
+import { mint } from "@trustvc/trustvc";
 
 const { trace } = getLogger("token-registry:issue");
 
@@ -22,29 +21,29 @@ export const issueToTokenRegistry = async ({
   ...rest
 }: TokenRegistryIssueCommand): Promise<TransactionReceipt> => {
   const wallet = await getWalletOrSigner({ network, ...rest });
-  const tokenRegistry: TradeTrustToken = await TradeTrustToken__factory.connect(address, wallet);
-  const encryptedRemark = validateAndEncryptRemark(remark, encryptionKey);
+  let transactionOptions: { maxFeePerGas?: BigNumber; maxPriorityFeePerGas?: BigNumber } = {};
+
   if (dryRun) {
-    await dryRunMode({
-      estimatedGas: await tokenRegistry.estimateGas.mint(beneficiary, holder, tokenId, encryptedRemark),
-      network,
-    });
+    console.log("🔧 Dry run mode is currently undergoing upgrades and will be available soon.");
     process.exit(0);
   }
 
-  let transaction;
   if (canEstimateGasPrice(network)) {
     const gasFees = await getGasFees({ provider: wallet.provider, ...rest });
     trace(`Gas maxFeePerGas: ${gasFees.maxFeePerGas}`);
     trace(`Gas maxPriorityFeePerGas: ${gasFees.maxPriorityFeePerGas}`);
-    await tokenRegistry.callStatic.mint(beneficiary, holder, tokenId, encryptedRemark);
-    signale.await(`Sending transaction to pool`);
-    transaction = await tokenRegistry.mint(beneficiary, holder, tokenId, encryptedRemark, { ...gasFees });
-  } else {
-    await tokenRegistry.callStatic.mint(beneficiary, holder, tokenId, encryptedRemark);
-    signale.await(`Sending transaction to pool`);
-    transaction = await tokenRegistry.mint(beneficiary, holder, tokenId, encryptedRemark);
+    transactionOptions = {
+      maxFeePerGas: gasFees.maxFeePerGas as BigNumber,
+      maxPriorityFeePerGas: gasFees.maxPriorityFeePerGas as BigNumber,
+    };
   }
+
+  const transaction = await mint(
+    { tokenRegistryAddress: address },
+    wallet,
+    { beneficiaryAddress: beneficiary, holderAddress: holder, tokenId, remarks: remark },
+    { id: encryptionKey, ...transactionOptions }
+  );
 
   trace(`Tx hash: ${transaction.hash}`);
   trace(`Block Number: ${transaction.blockNumber}`);
